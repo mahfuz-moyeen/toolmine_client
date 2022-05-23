ad5f5c4b0a5f7acefaf04cfdcb0a3dd77daca14f
mahfuzmoyeen10 <mahfuzzm10@gamil.com>
2022-05-23 15:46:08 +0600

    add product 80%

--- src/components/Pages/Dashboard/AddProduct.js
+++ src/components/Pages/Dashboard/AddProduct.js
@@ -1,13 +1,176 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
+import auth from '../../../firebase.init';
 
 const AddProduct = () => {
+    const [user] = useAuthState(auth);
+
+    const { register, formState: { errors }, handleSubmit, reset } = useForm();
+
+    const onSubmit = async data => {
+        console.log(data);
+    }
+
     return (
         <div className='w-11/12 mx-auto'>
-            <h1 className='text-white text-center text-3xl font-semibold my-10'>
-                <span className='p-1 border-b-2 border-primary'>Add <span className='text-primary'>Product</span></span>
-            </h1>
-            <div>
-                
+            <div className='py-5'>
+                <div className='card mx-auto'>
+                    <div className=' card-body bg-white'>
+                        <h1 className='text-center text-3xl font-semibold'>
+                            <span className='p-1 border-b-2 border-primary'>Add <span className='text-primary'>Product</span></span>
+                        </h1>
+                        <h1 className='card-title my-2 justify-center'>Hey{user?.displayName},  Your want to add your product?
+                        </h1>
+
+                        <form
+                            onSubmit={handleSubmit(onSubmit)}
+                            className='grid grid-cols-1 lg:grid-cols-2 gap-3'
+                        >
+
+                            <div>
+                                {/*  Product name  */}
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Product Name</span>
+                                    </label>
+                                    <input
+                                        type="text"
+                                        placeholder="Your Product Name"
+                                        className="input input-bordered bg-white"
+                                        {...register("name", {
+                                            required: {
+                                                value: true,
+                                                message: 'Product Name is Required'
+                                            }
+                                        })}
+                                    />
+                                    <label className="label">
+                                        {errors.name?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
+                                    </label>
+                                </div>
+
+                                {/* image  */}
+                                <div className="form-control w-full max-w-xs">
+                                    <label className="label">
+                                        <span className="label-text">Product Image</span>
+                                    </label>
+                                    <input
+                                        type="file"
+                                        className="bg-white"
+                                        {...register("img", {
+                                            required: {
+                                                value: true,
+                                                message: 'Image is Required'
+                                            }
+                                        })}
+                                    />
+                                    <label className="label">
+                                        {errors.img?.type === 'required' && <span className="label-text-alt text-red-500">{errors.img.message}</span>}
+                                    </label>
+                                </div>
+
+                                {/* description  */}
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Description</span>
+                                    </label>
+                                    <textarea
+                                        type="text"
+                                        placeholder="Your Product Description"
+                                        className="textarea border-2 h-40 border-gray-400  bg-white"
+                                        {...register("description", {
+                                            required: {
+                                                value: true,
+                                                message: 'Description is Required'
+                                            },
+                                            maxLength: {
+                                                value: 250,
+                                                message: 'You use maximum 250 characters'
+                                            }
+                                        })}
+                                    />
+                                    <label className="label">
+                                        {errors.description?.type === 'required' && <span className="label-text-alt text-red-500">{errors.description.message}</span>}
+
+                                        {errors.description?.type === 'maxLength' && <span className="label-text-alt text-red-500">{errors.description.message}</span>}
+                                    </label>
+                                </div>
+                            </div>
+
+
+                            <div>
+                                {/* price  */}
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Product Price ( per item )</span>
+                                    </label>
+                                    <input
+                                        type="number"
+                                        placeholder="Your Product Price $"
+                                        className="input input-bordered bg-white"
+                                        {...register("price", {
+                                            required: {
+                                                value: true,
+                                                message: 'Product Price is Required'
+                                            }
+                                        })}
+                                    />
+                                    <label className="label">
+                                        {errors.price?.type === 'required' && <span className="label-text-alt text-red-500">{errors.price.message}</span>}
+                                    </label>
+                                </div>
+
+                                {/* minQuantity  */}
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Minimum Quantity</span>
+                                    </label>
+                                    <input
+                                        type="number"
+                                        placeholder="Set your product minimum value"
+                                        className="input input-bordered bg-white"
+                                        {...register("minQuantity", {
+                                            required: {
+                                                value: true,
+                                                message: 'Product Minimum Quantity Price is Required'
+                                            }
+                                        })}
+                                    />
+                                    <label className="label">
+                                        {errors.minQuantity?.type === 'required' && <span className="label-text-alt text-red-500">{errors.minQuantity.message}</span>}
+                                    </label>
+                                </div>
+
+                                {/* availableQuantity  */}
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Available Quantity</span>
+                                    </label>
+                                    <input
+                                        type="number"
+                                        placeholder="Set your product available value"
+                                        className="input input-bordered bg-white"
+                                        {...register("availableQuantity", {
+                                            required: {
+                                                value: true,
+                                                message: 'Product Minimum Quantity Price is Required'
+                                            }
+                                        })}
+                                    />
+                                    <label className="label">
+                                        {errors.availableQuantity?.type === 'required' && <span className="label-text-alt text-red-500">{errors.availableQuantity.message}</span>}
+                                    </label>
+                                </div>
+
+                                <div className="form-control mt-6">
+                                    <input type='submit' value='ADD Product' className="btn btn-accent" />
+                                </div>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     );
